deb61fac3ea0f1ce8cff40e490844955e982b209
olivia-loup <olivia.loup@heig-vd.ch>
2024-11-21 20:13:24 +0100

    ex 2.1

--- mse_demo/software/sobel/obj/default/main.d
+++ mse_demo/software/sobel/obj/default/main.d
@@ -3,7 +3,14 @@ obj/default/main.o: main.c \
  C:/Users/olivi/master/embHard/mse_embHard_phase2/mse_demo/software/sobel_bsp/linker.h \
  c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\io.h \
  c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\alt_types.h \
- lcd_simple.h grayscale.h i2c.h camera.h vga.h dipswitch.h sobel.h
+ lcd_simple.h grayscale.h i2c.h camera.h vga.h dipswitch.h sobel.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_timestamp.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\alt_types.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\drivers\inc\altera_avalon_timer.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_dev.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_llist.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\priv\alt_dev_llist.h \
+ c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_warning.h
 
 C:/Users/olivi/master/embHard/mse_embHard_phase2/mse_demo/software/sobel_bsp/system.h:
 
@@ -26,3 +33,17 @@ vga.h:
 dipswitch.h:
 
 sobel.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_timestamp.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\alt_types.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\drivers\inc\altera_avalon_timer.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_dev.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_llist.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\priv\alt_dev_llist.h:
+
+c:\users\olivi\master\embhard\mse_embhard_phase2\mse_demo\software\sobel_bsp\hal\inc\sys\alt_warning.h:
---
+++ result_val_time.md
@@ -0,0 +1,48 @@
+# Result time in cycle 
+## 1
+### -O1
+sobel x m2 575815945
+sobel x def 4164991363
+sobel y 4165013458
+sobel threshold 4261351391
+sobel conv grayscale in mode 4 : 4249677245
+sobel x m2 575815945
+sobel x def 4164990274
+sobel y 4165013600
+sobel threshold 4261205951
+sobel conv grayscale in mode 4 : 4250549851
+
+### -O2
+sobel x m2 4294966786
+sobel x def 4163941979
+sobel y 4163957587
+sobel threshold 4256509671
+sobel conv grayscale in mode 4 : 4245442129
+sobel x m2 4294966786
+sobel x def 4164268680
+sobel y 4164269120
+sobel threshold 4256567536
+sobel conv grayscale in mode 4 : 4249800037
+### -O3
+sobel x m2 0
+sobel x def 4265610867
+sobel y 4267785761
+sobel threshold 4263045741
+sobel conv grayscale in mode 4 : 4250695232
+sobel x m2 0
+sobel x def 4265593001
+sobel y 4267967168
+sobel threshold 4263144296
+sobel conv grayscale in mode 4 : 4250886083
+## 2
+### unrolling innerloop
+sobel x m2 3
+sobel x def 3566869407
+sobel y 3563139885
+sobel threshold 4189122510
+sobel conv grayscale in mode 4 : 4181384688
+sobel x m2 3
+sobel x def 3562710178
+sobel y 3571428698
+sobel threshold 4191564118
+sobel conv grayscale in mode 4 : 4163372769
